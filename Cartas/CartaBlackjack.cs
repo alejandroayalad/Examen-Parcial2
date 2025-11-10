3c3766040b773dd3a8df11062cebbfc289fa89a7
alejandroayalad <alx.ayala08@gmail.com>
2025-11-10 14:58:25 -0600

    Agregue_Clase_Blackjack

--- Cartas/CartaBlackjack.cs
+++ Cartas/CartaBlackjack.cs
@@ -28,7 +28,40 @@ As
 
 
 
-public class CartaBlackjack
+public class cartaBlackjack : ICarta
 {
-  //LOL
+    private Palo _palo;
+    private Valor _valor;
+
+    public cartaBlackjack(Palo palo, Valor valor)
+    {
+        _palo = palo;
+        _valor = valor;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine(this.ToString());
+    }
+
+    public int ObtenerPuntaje()
+    {
+        if (_valor == Valor.Jack || _valor == Valor.Queen || _valor == Valor.King)
+        {
+            return 10;
+        }
+        else if (_valor == Valor.As)
+        {
+            return 11;
+        }
+        else
+        {
+            return (int)_valor;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Carta de {_valor} de {_palo}";
+    }
 }
