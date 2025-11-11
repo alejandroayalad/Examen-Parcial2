82e34efd90cff85c5c0d6dc83c916df62f6cfa84
TheMike69 <migue.2006.ortiz@gmail.com>
2025-11-11 12:11:51 -0600

    Implemente Jugador BJ y aqrquitectura de Dealer

--- Jugador/JugadorBlackjack/Dealer.cs
+++ Jugador/JugadorBlackjack/Dealer.cs
@@ -8,5 +8,14 @@ public class Dealer : JugadorBlackjack
     public Dealer(int id, string nombre) : base(id, nombre)
     {
     }
+    public void RepartirRonda(MazoDeCartas mazo, List<JugadorBlackjack> jugadores )
+    {
+
+    }
+    public override void JugarTurno(IContextoBlackJack contexto)
+    {
+
+    }
+
     
 }
--- Jugador/JugadorBlackjack/JugadorBlackjack.cs
+++ Jugador/JugadorBlackjack/JugadorBlackjack.cs
@@ -1,12 +1,56 @@
 using System;
+using System.Security.Cryptography;
 
 
 
-public class JugadorBlackjack : Jugador
+public abstract class JugadorBlackjack : Jugador // Lo mínimo de Jugador BlackJack
 {
 
-    protected JugadorBlackjack(int id, string nombre) : base(id, nombre)
+    public JugadorBlackjack(int id, string nombre) : base(id, nombre)
     {
-        
+        // Base viene de Jugador
+    }
+
+    public abstract void JugarTurno(IContextoBlackJack contexto);
+
+    public int ObtenerPuntuajeMano() //Para la sumatoria de los valores de las cartas del Jugador
+    {
+        int puntuajeBase = 0;
+        int numeroDeAses = 0;
+
+        foreach (var carta in MiMano.Cartas)
+        {
+            if (carta is cartaBlackjack cartaBj)
+            {
+                int valorCarta = cartaBj.ObtenerPuntaje();// Es de carta
+                if (valorCarta == 11)
+                {
+                    numeroDeAses++;
+                }
+               // Checa si es As
+                else
+                {
+                    puntuajeBase += valorCarta;
+
+                }
+                //Si no es, suma el puntuaje de los Enums
+            }
+        }
+        for (int i= 0; i < numeroDeAses; i++) //Para ver cuando usar 1 y 11 para As
+        {
+            if (puntuajeBase + 11 > 21)
+            {
+                puntuajeBase += 1;
+            }
+            else
+            {
+                puntuajeBase += 11;
+            }
+        }
+        return puntuajeBase;
+    }
+    public bool SePasoDe21()//Acaba el juego para el jugador que tiene más de 21
+    {
+        return false;
     }
 }
