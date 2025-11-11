5dd2d45d01f374527dbf790c210bee91ee41752d
TheMike69 <migue.2006.ortiz@gmail.com>
2025-11-10 23:16:23 -0600

    Cambios en jugador

--- Jugador/Jugador.cs
+++ Jugador/Jugador.cs
@@ -2,7 +2,42 @@ using System;
 
 namespace Examen_Juego.Jugador;
 
-public class Jugador
+public abstract class Jugador
 {
+    private int _id;
+    public int Id
+    {
+        get { return _id; }
+    }
 
+    protected Mano miMano;
+    protected string _nombre;
+
+    public string Nombre
+    {
+        get { return _nombre; }
+    }
+
+    protected Jugador(int id, string nombre)
+    {
+        _id = id;
+        _nombre = nombre;
+        miMano = new Mano();
+    }
+    public virtual void AñadirCartaAMano(ICarta cartaParaAñadir)
+    {
+
+    }
+    public virtual void MostrarEstado()
+    {
+
+    }
+    public virtual List<ICarta> LimpiarMano()
+    {
+
+    }
+    public int ObtenerConteoCartas()
+    {
+
+    }
 }
