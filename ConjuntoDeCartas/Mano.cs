ba76a2f329a1bc9150933f2c5216da222368de07
alejandroayalad <alx.ayala08@gmail.com>
2025-11-10 21:00:18 -0600

    cambio_archivo_Correcto

--- ConjuntoDeCartas/Mano.cs
+++ ConjuntoDeCartas/Mano.cs
@@ -1,8 +1,28 @@
 using System;
+using Examen_Parcial;
 
 
 
-public class Mano
-{
+public class Mano : ConjuntoDeCartas
+    {
+        public Mano() : base()
+        {
 
-}
+        }
+
+        public ICarta RemoverCarta(ICarta carta)
+        {
+            if (cartas.Remove(carta))
+            {
+                return carta;
+            }
+            throw new Exception("La carta no está en la mano.");
+        }
+        
+        public List<ICarta> Limpiar()
+        {
+            List<ICarta> cartasLimpiadas = new List<ICarta>(cartas);
+            cartas.Clear();
+            return cartasLimpiadas;
+        }
+    }
