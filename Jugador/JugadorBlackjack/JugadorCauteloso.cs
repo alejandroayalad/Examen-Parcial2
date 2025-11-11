28a2cd6a312547282f533282eb0be4018e1d4527
Julio-Santamaria <15245828@modelo.edu.mx>
2025-11-11 17:02:38 -0600

    Se agrego el comportamiento de jugador cauteloso BJ

--- Jugador/JugadorBlackjack/JugadorCauteloso.cs
+++ Jugador/JugadorBlackjack/JugadorCauteloso.cs
@@ -12,5 +12,16 @@ public class JugadorCauteloso : JugadorBlackjack
         _umbralPuntaje = umbral;
     }
 
+    public override void JugarTurno(IContextoBlackjack contexto)
+    {
+          Console.WriteLine($"{Nombre} está jugando su turno.");
+        while (ObtenerPuntajeMano() < _umbralPuntaje)
+        {
+            ICarta cartaRobada = contexto.RobarCartaDelMazo();
+            this.AñadirCartaAMano(cartaRobada);
+            Console.WriteLine($"{Nombre} roba una carta: {cartaRobada}");
+        }
+        Console.WriteLine($"{Nombre} ha terminado su turno.");
+    }   
 }
 
