486dcb54c308100064e9f418a66f6f9eacc30256
Julio-Santamaria <15245828@modelo.edu.mx>
2025-11-10 18:37:17 -0600

    Se agrego clase abstracta conjunto de cartas
    Se agregaron metodos y se  creo lista<ICartas> cartas

--- ConjuntoDeCartas/ConjuntoDeCartas.cs
+++ ConjuntoDeCartas/ConjuntoDeCartas.cs
@@ -1,8 +1,44 @@
 using System;
+using System.Collections.Generic;
 
+namespace Examen_Parcial;
 
-
-public class ConjuntoDeCartas
+public abstract class ConjuntoDeCartas
 {
 
+    protected List<ICarta> cartas;
+
+    protected static Random random = new Random();
+
+
+    protected ConjuntoDeCartas()
+    {
+        cartas = new List<ICarta>();
+    }
+
+    public IEnumerable<ICarta> Cartas
+    {
+        get { return cartas; }
+    }
+
+    public virtual void AgregarCarta(ICarta carta)
+    {
+        cartas.Add(carta);
+    }
+
+    public virtual int ContarCartas()
+    {
+        return cartas.Count;
+    }
+
+    public virtual bool EstaVacio()
+    {
+        return cartas.Count == 0;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(", ", cartas);
+    }
+    
 }
