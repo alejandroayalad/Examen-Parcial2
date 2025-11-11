d9407d762dfbf9bf94a544362593ecad4695b0d6
Julio-Santamaria <15245828@modelo.edu.mx>
2025-11-10 19:18:35 -0600

    Se agrego public class mano,
    hereda de conjunto de cartas. Incluye Icarta removercarta (remueve carta de mano) y Limpiar(), el cual limpia la mano (remueve tus cartas en cada nueva ronda de BJ)

--- ConjuntoDeCartas/ConjuntoDeCartas.cs
+++ ConjuntoDeCartas/ConjuntoDeCartas.cs
@@ -41,4 +41,28 @@ public abstract class ConjuntoDeCartas
         return string.Join(", ", cartas);
     }
     
+
+    public class Mano: ConjuntoDeCartas
+    {
+        public Mano() : base()
+        {
+
+        }
+
+        public ICarta RemoverCarta(ICarta carta)
+        {
+            if (cartas.Remove(carta))
+            {
+                return carta;
+            }
+            throw new Exception("La carta no está en la mano.");
+        }
+        
+        public List<ICarta> Limpiar()
+        {
+            List<ICarta> cartasLimpiadas = new List<ICarta>(cartas);
+            cartas.Clear();
+            return cartasLimpiadas;
+        }
+    }
 }
