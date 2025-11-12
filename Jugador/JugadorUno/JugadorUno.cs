ccc791c1fbc831c06ece31637e3d3ef7f51ae011
alejandroayalad <alx.ayala08@gmail.com>
2025-11-11 18:49:05 -0600

    CambieLógicaParaColor

--- Jugador/JugadorUno/JugadorUno.cs
+++ Jugador/JugadorUno/JugadorUno.cs
@@ -9,10 +9,44 @@ public abstract class JugadorUno : Jugador
     {
         
     }
-    protected Color ElegirColorAleatorio()
+    protected Color ElegirMejorColorSegunMano()
     {
-        int colorIndex = random.Next(4);
-        return (Color)colorIndex;
+        var conteoPorColor = new Dictionary<Color, int>
+    {
+        { Color.Rojo, 0 }, { Color.Azul, 0 }, { Color.Verde, 0 }, { Color.Amarillo, 0 }
+    };
+
+        foreach (var carta in MiMano.Cartas)
+        {
+            if (carta is CartaUno cartaUno && cartaUno.Color.HasValue)
+            {
+                conteoPorColor[cartaUno.Color.Value]++;
+            }
+        }
+
+
+        Color? mejorColor = null;
+        int maxConteo = -1;
+
+        foreach (var par in conteoPorColor)
+        {
+
+            if (par.Value > maxConteo)
+            {
+                maxConteo = par.Value;
+                mejorColor = par.Key;
+            }
+        }
+
+
+        if (maxConteo <= 0 || !mejorColor.HasValue)
+        {
+            Console.WriteLine($" {Nombre} no tiene cartas de color, elige al azar.");
+            return (Color)random.Next(0, 4);
+        }
+
+        Console.WriteLine($"{Nombre} elige {mejorColor.Value} (tiene {maxConteo} cartas).");
+        return mejorColor.Value;
     }
 
 
